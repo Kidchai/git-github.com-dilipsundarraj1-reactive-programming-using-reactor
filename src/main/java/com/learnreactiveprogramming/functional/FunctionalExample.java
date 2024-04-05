db6223ace86de304728529df90b358dedd1c6dcc
Kidchai <o.naibova@gmail.com>
2024-04-05 14:42:01 -0500

    Add example Imperative vs Functional style

--- src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
+++ src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
@@ -1,4 +1,21 @@
 package com.learnreactiveprogramming.functional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FunctionalExample {
+    public static void main(String[] args) {
+        var names = List.of("Oleg", "Sam", "Barbara", "Katy", "Tom", "Barbara");
+        var filteredNames = returnNamesGreaterThanSizeNoDuplicatesUppercaseSorted(names, 3);
+        System.out.println(filteredNames);
+    }
+
+    private static List<String> returnNamesGreaterThanSizeNoDuplicatesUppercaseSorted(List<String> names, int size) {
+        return names.stream()
+                .filter(name -> name.length() > size)
+                .distinct()
+                .map(String::toUpperCase)
+                .sorted()
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
+++ src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
@@ -1,4 +1,23 @@
 package com.learnreactiveprogramming.imperative;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ImperativeExample {
+    public static void main(String[] args) {
+        var names = List.of("Oleg", "Sam", "Barbara", "Katy", "Tom", "Barbara");
+        var filteredNames = returnNamesGreaterThanSizeNoDuplicatesUppercaseSorted(names, 3);
+        System.out.println(filteredNames);
+    }
+
+    private static List<String> returnNamesGreaterThanSizeNoDuplicatesUppercaseSorted(List<String> names, int size) {
+        var filteredNames = new ArrayList<String>();
+        for (var name : names) {
+            if (name.length() > size && !filteredNames.contains(name.toUpperCase()))
+                filteredNames.add(name.toUpperCase());
+        }
+        Collections.sort(filteredNames);
+        return filteredNames;
+    }
 }
